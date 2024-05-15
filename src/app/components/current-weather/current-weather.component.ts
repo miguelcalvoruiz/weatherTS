c29da6597efdbc367c60f618091967fd5f64fd60
Miguel <miguelcalvoruiz6@gmail.com>
2024-05-15 20:29:19 +0200

    animation backgrounds to current weather

--- src/app/components/current-weather/current-weather.component.ts
+++ src/app/components/current-weather/current-weather.component.ts
@@ -1,7 +1,28 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { WeatherApiService } from '../../services/weather-api/weather-api.service';
 import { UtilityService } from '../../services/utility/utility.service';
-import { ActivatedRoute } from '@angular/router';
+
+
+interface WeatherVideos {
+  Clear: string;
+  Clouds: string;
+  Rain: string;
+  Drizzle: string;
+  Thunderstorm: string;
+  Snow: string;
+  Mist: string;
+  Smoke: string;
+  Haze: string;
+  Dust: string;
+  Fog: string;
+  Sand: string;
+  Ash: string;
+  Squall: string;
+  Tornado: string;
+  [key: string]: string;
+}
+
+
 
 @Component({
   selector: 'app-current-weather',
@@ -9,15 +30,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
-  lat!: number;
-  lon!: number;
-
+  @ViewChild('animationContainer', { static: true }) animationContainer!: ElementRef;
   currentWeatherData: any;
+  backgroundVideo!: string;
 
   constructor(
     private weatherApiService: WeatherApiService,
-    private utilityService: UtilityService,
-    private route: ActivatedRoute
+    private utilityService: UtilityService
   ) { }
 
   ngOnInit(): void {
@@ -38,6 +57,31 @@ export class CurrentWeatherComponent implements OnInit {
         date: this.utilityService.getDate(data.dt, data.timezone),
         location: `${data.name}, ${data.sys.country}`
       };
+      const weatherCondition = data.weather[0].main;
+      this.backgroundVideo = this.getBackgroundVideo(weatherCondition);
     });
   }
+
+  getBackgroundVideo(weatherCondition: string): string {
+    const videos: WeatherVideos = {
+      Clear: 'clear-day.mp4',
+      Clouds: 'cloudy.mp4',
+      Rain: 'rainy.mp4',
+      Drizzle: 'rainy.mp4',
+      Thunderstorm: 'thunderstorm.mp4',
+      Snow: 'snowy-day.mp4',
+      Mist: 'haze.mp4',
+      Smoke: 'haze.mp4',
+      Haze: 'haze.mp4',
+      Dust: 'dust.mp4',
+      Fog: 'haze.mp4',
+      Sand: 'haze.mp4',
+      Ash: 'haze.mp4',
+      Squall: 'rainy.mp4',
+      Tornado: 'tornado.mp4'
+      
+    };
+    return videos[weatherCondition] || 'cloudy.mp4';
+  }
+  
 }
